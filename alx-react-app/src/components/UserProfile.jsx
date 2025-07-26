3575e15787aa9d61fdc85f8c6ed7b50756b81abb
Mzwakhe <lungelomahaye95@gmail.com>
2025-07-26 22:44:58 +0200

    i fixed the bio

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -13,8 +13,8 @@ const UserProfile = (props) => {
       backgroundColor: '#f9f9f9'
     }}>
       <h2>{props.name}</h2>
-      <p><strong>Age:</strong> {props.age}</p>
-      <p>{props.bio}</p>
+      <p><strong>Age: </strong> {props.age}</p>
+      <p><strong>Bio:</strong> {props.bio}</p>
     </div>
   );
 };
