52682468d43ef76cd6bfa7b406297c76ed2b70e7
Mzwakhe <lungelomahaye95@gmail.com>
2025-07-26 23:19:50 +0200

    Added styled components with inline CSS

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+ 
+
+  return (
+    <div>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      
+      <h1 style={{ textAlign: 'center' }}>User Profiles</h1>
+
+      {/* ✅ Use UserProfile with props */}
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+
+      <UserProfile 
+        name="John Doe" 
+        age="30" 
+        bio="Software engineer who enjoys coding and traveling" 
+      />
+      <Footer />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e0f7fa', textAlign: 'center' }}>
+      <p style={{ fontSize: '1.2rem' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
