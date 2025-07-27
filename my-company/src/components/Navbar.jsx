8bf5630ea31dced2b12a681fbbd1bcd780e025bb
Mzwakhe <lungelomahaye95@gmail.com>
2025-07-27 18:51:27 +0200

    i fixed the error on navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#333', padding: '10px', textAlign: 'center' }}>
+    <nav style={{ display: 'flex', justifyContent: 'space-around', backgroundColor: '#333', padding: '10px', textAlign: 'center' }}>
       <Link to="/" style={{ color: 'white', margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ color: 'white', margin: '0 10px' }}>Services</Link>
