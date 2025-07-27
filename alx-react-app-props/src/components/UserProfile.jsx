1d05909100c9ff928bf0e03dcb4ae7e421cea111
Mzwakhe <lungelomahaye95@gmail.com>
2025-07-27 18:13:39 +0200

    Refactored project to use Context API instead of prop drilling

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,22 @@
+// src/components/UserProfile.jsx
+import React from 'react';
+
+const UserProfile = (props) => {
+  return (
+    <div style={{
+      border: '1px solid #ccc',
+      padding: '20px',
+      borderRadius: '10px',
+      maxWidth: '300px',
+      margin: '20px auto',
+      textAlign: 'center',
+      backgroundColor: '#f9f9f9'
+    }}>
+      <h2>{props.name}</h2>
+      <p><strong>Age: </strong> {props.age}</p>
+      <p><strong>Bio:</strong> {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
