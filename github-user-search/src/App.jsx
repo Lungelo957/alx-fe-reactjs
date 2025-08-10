ec65650e938e830f2db7748d09eadfb896baa53c
Mzwakhe <lungelomahaye95@gmail.com>
2025-08-10 20:29:00 +0200

    i fixed the error on the Search.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import SearchBar from "./components/SearchBar";
+import Search from "./components/Search";
 import UserCard from "./components/UserCard";
 import { fetchUserData } from "./services/githubService";
 
@@ -26,7 +26,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold text-center mb-6">GitHub User Search</h1>
-      <SearchBar onSearch={handleSearch} />
+      <Search onSearch={handleSearch} />
 
       {loading && <p className="text-center">Loading...</p>}
       {error && <p className="text-center text-red-500">{error}</p>}
